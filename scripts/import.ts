5d98bd8fa025dac4226dc7749d6357ef685983cc
nijippoi <nijippoi@users.noreply.github.com>
2026-03-11 06:01:57 +0900

    初期実装

---
+++ scripts/import.ts
@@ -0,0 +1,257 @@
+import * as yaml from '@std/yaml';
+import * as path from '@std/path';
+import {
+  BaseRole,
+  GroupRole,
+  Person,
+  PersonRole,
+  Persons,
+  Role,
+} from '../src/types.ts';
+
+async function importYaml(dataDir: string, srcDir: string): Promise<void> {
+  // dirPath内のYAMLを読み込む
+  const allGroupsData = [];
+  const allPersonsData = [];
+  for await (const entry of Deno.readDir(dataDir)) {
+    if (entry.isFile && /\.(yaml|yml)$/.test(entry.name)) {
+      try {
+        const filePath = path.join(dataDir, entry.name);
+        const data: any = yaml.parse(Deno.readTextFileSync(filePath));
+        data.file = filePath;
+        switch (data.source_type) {
+          case 'groups':
+            allGroupsData.push(data);
+            break;
+          case 'persons':
+            allPersonsData.push(data);
+            break;
+          default:
+            console.warn(`Ignoring ${filePath}`);
+        }
+      } catch (error) {
+        console.error(`Error parsing ${entry.name}:`, error);
+      }
+    }
+  }
+
+  // TODO: ID重複チェック
+
+  const groups: { [key: string]: any } = {};
+  const groupQualifiers: { [key: string]: number } = {};
+  const groupParents: { [key: string]: string } = {};
+  const groupParentQualifiers: { [key: string]: string } = {};
+
+  // グループにIDを付与
+  for (const groupsData of allGroupsData) {
+    let groupId = groupsData.start_id;
+    for (const groupData of groupsData.groups) {
+      if (groupData.reserve_id_until) {
+        groupId = groupData['reserve_id_until']++;
+        continue;
+      }
+
+      let group: any = {
+        id: groupId.toString(),
+      };
+
+      // グループ名を正規化
+      if (groupData.names) {
+        group.names = structuredClone(groupData.names);
+      } else {
+        group.names = [{
+          name: groupData.name,
+          ...(groupData.name_kana && { name_kana: groupData.name_kana }),
+        }];
+      }
+      if (groupData.qualifier) {
+        if (groupQualifiers[groupData.qualifier]) {
+          console.error(
+            `Group qualifier ${groupData.qualifier} already exists as id ${
+              groupQualifiers[groupData.qualifier]
+            }`,
+          );
+        }
+        groupQualifiers[groupData.qualifier] = groupId;
+      }
+      if (groupData.parent && !groupData.parent_qualifier) {
+        groupParents[groupId] = groupData.parent;
+      }
+      if (!groupData.parent && groupData.parent_qualifier) {
+        groupParentQualifiers[groupId] = groupData.parent_qualifier;
+      }
+      if (groupData.active_date_ranges) {
+        group.active_date_ranges = structuredClone(
+          groupData.active_date_ranges,
+        );
+      }
+
+      // console.log(groupData);
+      groups[groupId.toString(10)] = group;
+      groupId++;
+    }
+  }
+  // 親グループを解決
+  for (const groupId in groupParents) {
+    const parentName = groupParents[groupId];
+    const parentIds: Set<string> = new Set();
+    for (const parentId in groups) {
+      for (const name of groups[parentId].names) {
+        if (parentName == name.name) parentIds.add(parentId);
+      }
+    }
+    if (parentIds.size === 1) {
+      groups[groupId].parent_id = parentIds.values().next().value;
+    } else if (parentIds.size === 0) {
+      console.error(
+        `Group parent ${parentName} not found for ${groups[groupId].names[0]}`,
+      );
+    } else {
+      console.error(
+        `Group parent ${parentName} has multiple candidates for ${
+          groups[groupId].names[0]
+        }. Use parent_qualifier instead.`,
+      );
+    }
+  }
+
+  Deno.mkdirSync(srcDir, { recursive: true });
+  Deno.writeTextFileSync(
+    path.join(srcDir, 'groups.json'),
+    JSON.stringify(groups),
+  );
+  Deno.writeTextFileSync(
+    path.join(srcDir, 'groups.pretty.json'),
+    JSON.stringify(groups, null, 2),
+  );
+
+  const resolveGroup = (
+    name?: string,
+    qualifier?: string,
+  ): string | undefined => {
+    if (qualifier && groupQualifiers.hasOwn(qualifier)) {
+      return groupQualifiers[qualifier].toString();
+    }
+    const groupIds: Set<string> = new Set();
+    for (const groupId in groups) {
+      for (const groupName of groups[groupId].names) {
+        if (name == groupName.name) groupIds.add(groupId);
+      }
+    }
+    if (groupIds.size === 1) {
+      return groupIds.values().next().value;
+    } else if (groupIds.size === 0) {
+      console.error(
+        `Group ${name} not found`,
+      );
+    } else {
+      console.error(
+        `Group ${name} has multiple candidates. Use group_qualifier instead.`,
+      );
+    }
+    return undefined;
+  };
+
+  const persons: Persons = {};
+
+  // 人物にIDを付与
+  for (const datas of allPersonsData) {
+    let id = datas.start_id;
+    for (const data of datas.persons) {
+      if (data.reserve_id_until) {
+        id = data['reserve_id_until']++;
+        continue;
+      }
+
+      const person: Person = { id: id.toString() };
+
+      // 人名正規化
+      if (data.names) {
+        person.names = structuredClone(data.names);
+      } else {
+        person.names = [{
+          ...(data.full_name && { full_name: data.full_name }),
+          ...(data.full_name_kana &&
+            { full_name_kana: data.full_name_kana }),
+          ...(data.family_name && { family_name: data.family_name }),
+          ...(data.family_name_kana &&
+            { family_name_kana: data.family_name_kana }),
+          ...(data.given_name && { given_name: data.given_name }),
+          ...(data.given_name_kana &&
+            { given_name_kana: data.given_name_kana }),
+          ...(data.middle_name && { middle_name: data.middle_name }),
+          ...(data.middle_name_kana &&
+            { middle_name_kana: data.middle_name_kana }),
+        }];
+      }
+      if (data.birth_date) {
+        person.birth_date = data.birth_date;
+      }
+      if (data.birth_place) {
+        person.birth_place = structuredClone(data.birth_place);
+      }
+      if (data.hometown) {
+        person.hometown = structuredClone(data.hometown);
+      }
+      if (data.roles) {
+        person.roles = [];
+        for (const roleData of data.roles) {
+          if (!roleData.role) {
+            console.error(`Role is not set on person id ${id}`);
+            continue;
+          }
+          if (roleData.group || roleData.group_qualifier) {
+            const groupId = resolveGroup(
+              roleData.group,
+              roleData.group_qualifier,
+            );
+            if (groupId) {
+              const role: GroupRole = {
+                role: roleData.role,
+                group_id: groupId,
+              };
+              if (roleData.active_date_ranges) {
+                role.active_date_ranges = structuredClone(
+                  roleData.active_date_ranges,
+                );
+              }
+              person.roles.push(role);
+            }
+          } else {
+            const role: PersonRole = {
+              role: roleData.role,
+              person_id: id.toString(),
+            };
+            if (roleData.active_date_ranges) {
+              role.active_date_ranges = structuredClone(
+                roleData.active_date_ranges,
+              );
+            }
+            person.roles.push(role);
+          }
+        }
+      }
+
+      // console.log(groupData);
+      persons[id.toString(10)] = person;
+      id++;
+    }
+  }
+
+  Deno.mkdirSync(srcDir, { recursive: true });
+  Deno.writeTextFileSync(
+    path.join(srcDir, 'persons.json'),
+    JSON.stringify(persons),
+  );
+  Deno.writeTextFileSync(
+    path.join(srcDir, 'persons.pretty.json'),
+    JSON.stringify(persons, null, 2),
+  );
+}
+
+if (import.meta.main) {
+  await importYaml(
+    Deno.args[0] || path.join(Deno.cwd(), 'data'),
+    Deno.args[1] || path.join(Deno.cwd(), 'src', 'data'),
+  );
+}
---
+++ src/types.ts
@@ -0,0 +1,225 @@
+/**
+ * アーティストグループデータベース型定義
+ */
+
+/** グループのエンティティのID */
+export const GROUP_ENTITY_ID = 1;
+
+/** アーティストのエンティティのID */
+export const PERSON_ENTITY_ID = 2;
+
+export type Id = string;
+
+/**
+ * ISO 8601形式の文字列 (YYYY-MM-DD)
+ */
+export type DateString = `${number}${number}${number}${number}-${
+  | '01'
+  | '02'
+  | '03'
+  | '04'
+  | '05'
+  | '06'
+  | '07'
+  | '08'
+  | '09'
+  | '10'
+  | '11'
+  | '12'}-${
+  | '01'
+  | '02'
+  | '03'
+  | '04'
+  | '05'
+  | '06'
+  | '07'
+  | '08'
+  | '09'
+  | '10'
+  | '11'
+  | '12'
+  | '13'
+  | '14'
+  | '15'
+  | '16'
+  | '17'
+  | '18'
+  | '19'
+  | '20'
+  | '21'
+  | '22'
+  | '23'
+  | '24'
+  | '25'
+  | '26'
+  | '27'
+  | '28'
+  | '29'
+  | '30'
+  | '31'}`;
+
+/**
+ * 開始日と終了日を含む期間
+ */
+export interface DateRange {
+  /** 開始日(含む): ISO 8601形式 (YYYY-MM-DD) */
+  start?: DateString;
+  /** 終了日(含む): ISO 8601形式 (YYYY-MM-DD) */
+  end?: DateString;
+}
+
+/**
+ * 活動期間を持つエンティティのインターフェース
+ */
+export interface HasActiveDateRanges {
+  /** 有効期間の配列 */
+  active_date_ranges?: DateRange[];
+}
+
+/**
+ * 削除フラグを持つエンティティのインターフェース
+ */
+export interface HasDeleted {
+  /** 削除フラグ */
+  deleted: true;
+}
+
+/**
+ * IDを持つエンティティのインターフェース
+ */
+export interface HasId {
+  /** ID */
+  id: Id;
+}
+
+/**
+ * 都道府県などの地域情報
+ */
+export interface Location {
+  /** ISO 3166-1 alpha-2国コード */
+  country?: string;
+  /** 国内の最上位の行政区域に当たるもの。例えば日本だと都道府県名になる。 */
+  state?: string;
+  /** オプショナルな市区町村名 */
+  city?: string;
+}
+
+/**
+ * グループ名のバリエーション（期間指定可能）
+ */
+export interface GroupName {
+  /** グループ名 */
+  name: string;
+  /** グループ名のカタカナ読み */
+  name_kana?: string;
+}
+
+/**
+ * グループ定義（メイングループまたはサブグループ）
+ */
+export interface Group extends GroupName, HasId, HasActiveDateRanges {
+  /** グループ名の配列 */
+  names: (GroupName & HasActiveDateRanges)[];
+  /** サブグループの親グループ名 */
+  parent_id?: Id;
+}
+
+/**
+ * グループ一覧
+ */
+export interface Groups {
+  [key: Id]: Group;
+}
+
+/**
+ * 共通のロール情報
+ */
+export interface BaseRole extends HasActiveDateRanges {
+  /** 役割名 */
+  role:
+    | 'solo'
+    | 'member'
+    | 'trainee'
+    | 'leader'
+    | 'subleader'
+    | 'vocal'
+    | 'dancer'
+    | 'producer'
+    | 'drummer'
+    | 'guitarist'
+    | 'keyboardist';
+}
+
+/**
+ * グループロール（グループに関連する役職）
+ */
+export interface GroupRole extends BaseRole {
+  /** グループID */
+  group_id: Id;
+}
+
+/**
+ * アーティストロール（アーティストに関連する役職）
+ */
+export interface PersonRole extends BaseRole {
+  /** アーティストID */
+  person_id: Id;
+}
+
+/**
+ * 役割と活動期間（グループまたはアーティストいずれか一方に関連）
+ */
+export type Role = GroupRole | PersonRole;
+
+/**
+ * 役職定義（利用可能な役職についてのメタデータ）
+ */
+export interface RoleType {
+  /** 役職の表示名 */
+  name: string;
+}
+
+/**
+ * 人名（期間指定可能）
+ */
+export interface PersonName {
+  /** フルネーム */
+  name?: string;
+  /** フルネームのカタカナ読み */
+  name_kana?: string;
+  /** 苗字（漢字） */
+  family_name?: string;
+  /** 苗字（カタカナ） */
+  family_name_kana?: string;
+  /** 下の名前（漢字、または苗字がない場合は名前） */
+  given_name: string;
+  /** 下の名前（カタカナ） */
+  given_name_kana: string;
+  /** ミドルネーム（漢字） */
+  middle_name?: string;
+  /** ミドルネーム（カタカナ） */
+  middle_name_kana?: string;
+}
+
+/**
+ * 人物情報
+ */
+export interface Person extends HasId, HasActiveDateRanges {
+  /** 名前のバリエーション（期間指定可能） */
+  names?: (PersonName & HasActiveDateRanges)[];
+  /** 生年月日（ISO 8601形式: YYYY-MM-DD） */
+  birth_date?: DateString;
+  /** 出生地 */
+  birth_place?: Location;
+  /** 故郷 */
+  hometown?: Location;
+  /** 役割の配列 */
+  roles?: Role[];
+}
+
+/**
+ * 人物一覧
+ */
+export interface Persons {
+  [key: Id]: Person;
+}
